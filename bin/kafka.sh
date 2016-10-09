a80327e1c0843add164c82df3468c675e6a2b6bb
noprom <tyee.noprom@qq.com>
2016-10-09 17:49:07 +0800

    optimize kafka.sh

--- bin/kafka.sh
+++ bin/kafka.sh
@@ -2,11 +2,13 @@
 
 if [ $# -ne 2 ];
 then
-    echo "`basename ${0}`:usage: [-local start] | [-local stop] | [-local consume] | [-cluster start] | [-cluster stop] | [-cluster consume]"
+    echo "`basename ${0}`:usage: [-local start] | [-local stop] | [-local consume] | [-local list] | [-local delete] | [-local describe] | [-cluster start] | [-cluster stop] | [-cluster consume]"
 	exit 1
 fi
 option=$1
 action=$2
+zookeeper_cluster=localhost:2181/kafka
+topic=log-topic
 case ${option} in
     -local)
         if [ ${action} == 'start' ];
@@ -15,15 +17,25 @@ case ${option} in
                 exec sudo ${KAFKA_HOME}/bin/kafka-server-start.sh ${KAFKA_HOME}/config/server.properties &
                 exec sudo ${KAFKA_HOME}/bin/kafka-server-start.sh ${KAFKA_HOME}/config/server-1.properties &
                 exec sudo ${KAFKA_HOME}/bin/kafka-server-start.sh ${KAFKA_HOME}/config/server-2.properties &
-                exec sudo ${KAFKA_HOME}/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 2 --partitions 3 --topic log-topic
+                exec sudo ${KAFKA_HOME}/bin/kafka-topics.sh --zookeeper ${zookeeper_cluster} --create --replication-factor 1 --partitions 1 --topic ${topic} &
         elif [ ${action} == 'stop' ]
             then
+                exec sudo ${KAFKA_HOME}/bin/zookeeper-server-stop.sh &
                 exec sudo ${KAFKA_HOME}/bin/kafka-server-stop.sh ${KAFKA_HOME}/config/server.properties &
                 exec sudo ${KAFKA_HOME}/bin/kafka-server-stop.sh ${KAFKA_HOME}/config/server-1.properties &
-                exec sudo ${KAFKA_HOME}/bin/kafka-server-stop.sh ${KAFKA_HOME}/config/server-2.properties
+                exec sudo ${KAFKA_HOME}/bin/kafka-server-stop.sh ${KAFKA_HOME}/config/server-2.properties &
         elif [ ${action} == 'consume' ]
             then
-                exec sudo ${KAFKA_HOME}/bin/kafka-console-consumer.sh --zookeeper localhost:2181 --topic log-topic --from-beginning
+                exec sudo ${KAFKA_HOME}/bin/kafka-console-consumer.sh --zookeeper ${zookeeper_cluster} --topic log-topic --from-beginning
+        elif [ ${action} == 'list' ]
+            then
+                exec sudo ${KAFKA_HOME}/bin/kafka-topics.sh --zookeeper ${zookeeper_cluster} --list
+        elif [ ${action} == 'delete' ]
+            then
+                exec sudo ${KAFKA_HOME}/bin/kafka-topics.sh --zookeeper ${zookeeper_cluster} --delete --topic ${topic}
+        elif [ ${action} == 'describe' ]
+            then
+                exec sudo ${KAFKA_HOME}/bin/kafka-topics.sh --zookeeper ${zookeeper_cluster} --describe --topic ${topic}
         fi
         ;;
     -cluster)
